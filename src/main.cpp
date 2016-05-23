6ce9ef552f6a805ea12992c579f9139799d4d6e5
ProKil <prokilchu@gmail.com>
2016-05-23 14:28:21 +0800

    move main.cpp to src

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,41 @@
-main.cpp
-#include "word_lib"
+#include<iostream>
+#include<string>
+#include<vector>
+#include"src/word_library.h"
+#include"src/search_history_library.h"
+#include"src/review_history_library.h"
+
+using namespace std;
+extern word_library word_lib;
+
+int main()
+{
+	cout << "Loaded.\n";
+	word_library::iterator it;
+	it = word_lib.end()-1;
+	cout << it->get_word_name() << endl;
+        string nword;
+        cin >> nword;
+        while(nword != "xxxxx")
+	{
+		for(it = word_lib.begin(); it != word_lib.end(); it ++)
+		{
+			if(it->get_word_name() == nword)
+			{
+				word::iterator it2;
+				for(it2 = it->begin(); it2 != it->end(); it2 ++)
+				{
+					cout << nword << endl;
+					cout << it2->get_pos() << endl;
+					cout << it2->get_meaning() << endl;
+					cout << endl;
+				}
+				break;
+			}
+		}
+		if(it == word_lib.end()) cout << "Not found!\n";
+		cin >> nword;
+	}
+        cout << "Hello world!" << endl;
+        return 0;
+}
