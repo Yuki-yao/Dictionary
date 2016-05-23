921338bf74e47eba83b73aba8966f99e14d18fb6
ProKil <prokilchu@gmail.com>
2016-05-23 14:52:46 +0800

    clean my test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include"src/word_library.h"
-#include"src/search_history_library.h"
-#include"src/review_history_library.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include "word_library.h"
+#include "search_history_library.h"
+#include "review_history_library.h"
 
 using namespace std;
 extern word_library word_lib;
@@ -14,18 +14,18 @@ int main()
 	word_library::iterator it;
 	it = word_lib.end()-1;
 	cout << it->get_word_name() << endl;
-        string nword;
-        cin >> nword;
-        while(nword != "xxxxx")
+    string nword;
+    cin >> nword;
+    while(nword != "xxxxx")
 	{
 		for(it = word_lib.begin(); it != word_lib.end(); it ++)
 		{
 			if(it->get_word_name() == nword)
 			{
 				word::iterator it2;
+                cout << nword << endl;
 				for(it2 = it->begin(); it2 != it->end(); it2 ++)
 				{
-					cout << nword << endl;
 					cout << it2->get_pos() << endl;
 					cout << it2->get_meaning() << endl;
 					cout << endl;
@@ -33,9 +33,9 @@ int main()
 				break;
 			}
 		}
-		if(it == word_lib.end()) cout << "Not found!\n";
+		if(it == word_lib.end()) cout << "Not found!\n" << endl;
 		cin >> nword;
 	}
-        cout << "Hello world!" << endl;
-        return 0;
+    cout << "Hello world!" << endl;
+    return 0;
 }
